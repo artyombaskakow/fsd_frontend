32e504ede2202d142ad65576848dce2577acafb5
Artyom <artyom.baskakow@gmail.com>
2019-07-23 16:00:34 +0500

    Реализовал блок form-button-like

--- src/blocks/blocks.js
+++ src/blocks/blocks.js
@@ -1,1 +1,2 @@
 import './form-group/form-group';
+import './form-button-like/form-buttom-like';
---
+++ src/blocks/form-button-like/form-buttom-like.js
@@ -0,0 +1,26 @@
+(function(){
+    console.log('like btn');
+    let likeBtns = document.querySelectorAll('.form-button-like');
+
+    for(let i=0; i<likeBtns.length; i++){
+
+        likeBtns[i].addEventListener('click', function(){
+            let countElem = this.querySelector('.form-button-like__count');
+            let likeCount = +countElem.innerHTML;
+            let likedClassName = 'form-button-like_liked';
+    
+            if(this.classList.contains(likedClassName)){
+                this.classList.remove(likedClassName);
+                likeCount--;
+            }
+            else{
+                this.classList.add(likedClassName);
+                likeCount++;
+            }
+    
+            countElem.innerHTML = likeCount;
+        });
+    }
+
+    
+})();
