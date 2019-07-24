b4b49798f34aadc513c42182727b4501b444a964
Artyom <artyom.baskakow@gmail.com>
2019-07-25 02:24:49 +0500

    Замена svg-элементов на иконки material icons. Корректировка form-checkbox. Создан блок advantages-list

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './blocks/blocks'
 import './js/index';
+import '../node_modules/material-icons/iconfont/material-icons.scss'
 import './assets/css/main.css'
 import './assets/scss/main.scss'
