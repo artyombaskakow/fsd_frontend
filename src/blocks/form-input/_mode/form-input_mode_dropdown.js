fbd38f224f5340213ea04e3ff88e3c8efb0e5ef7
Artyom <artyom.baskakow@gmail.com>
2019-07-26 14:22:11 +0500

    Реализован режим dropdown для form-input

--- src/blocks/form-input/_mode/form-input_mode_dropdown.js
+++ src/blocks/form-input/_mode/form-input_mode_dropdown.js
@@ -1,49 +1,55 @@
 (function(){
     console.log('dropdown');
-
     let dropdownInputs = document.querySelectorAll('.form-input_mode_dropdown');
-    let dropdownOpenClasses = ['form-input_mode_dropdown', 'form-input__icon'];
 
-    let countElems = document.querySelectorAll('.form-input__dropdown-elem-minus, .form-input__dropdown-elem-plus');
-    for(let i=0; i<countElems.length; i++){
-        countElems[i].addEventListener('click', makeHandleCountClick);
+    dropdownInputsInit(dropdownInputs);
+
+
+    function dropdownInputsInit(dropdownInputs){
+
+        for(let i=0; i<dropdownInputs.length; i++){
+            dropdownInputs[i].addEventListener('click', handleDropdownInputClick);
+            let formInputContainer = dropdownInputs[i].querySelector('.form-input__container');
+            setInputText(formInputContainer);
+        }
+
+        let countElems = document.querySelectorAll('.form-input__dropdown-elem-minus, .form-input__dropdown-elem-plus');
+        for(let i=0; i<countElems.length; i++){
+            countElems[i].addEventListener('click', handleCountClick);
+        }
+
+        let applyButtons = document.querySelectorAll('.form-input__dropdown-apply');
+        for(let i=0; i<applyButtons.length; i++){
+            applyButtons[i].addEventListener('click', handleApplyClick);
+        }
+
+        let clearButtons = document.querySelectorAll('.form-input__dropdown-clear');
+        for(let i=0; i<clearButtons.length; i++){
+            clearButtons[i].addEventListener('click', handleClearClick);
+        }
+
+        document.body.addEventListener('click', handleBodyClick);
     }
+    
 
-    let hideDropdownInputs = function(){
-        let expandedDropdownInput = document.querySelectorAll('.form-input_expanded');
-        for(let i=0; i<expandedDropdownInput.length; i++){
-            expandedDropdownInput[i].classList.remove('form-input_expanded');
-        }
-    }
-
-    document.body.addEventListener('click', function(e){
-        //console.log('body', e);
-        for(let i=0; i<dropdownOpenClasses.length; i++){
-            if(e.target.classList.contains(dropdownOpenClasses[i])){
-                //console.log('contains');
-                return;
-            } 
-        }
-
-        let dropDownContainer = e.target.closest(".form-input__dropdown-elems-container");
-
-        if(dropDownContainer==null){
-            //hideDropdownInputs();
-        }
-    });
-
-    dropdownInputs[0].addEventListener('click', function(e){
-        //console.log(e);
+    function handleDropdownInputClick(e){
         if(e.target.classList.contains('form-input__input') || e.target.classList.contains('form-input__icon')){
             this.classList.toggle('form-input_expanded');
         }
-    });
+    }
+
+    function handleBodyClick(e){
+        let formInput = e.target.closest(".form-input_mode_dropdown");
+
+        for(let i=0; i<dropdownInputs.length; i++){
+            if(dropdownInputs[i]!=formInput){
+                dropdownInputs[i].classList.remove('form-input_expanded');
+            }
+        }
+    }
 
 
-
-
-
-    function makeHandleCountClick(){
+    function handleCountClick(){
 
         let inputElem = this.parentNode.querySelector('.form-input__dropdown-elem-input');
         let count = inputElem.value = this.classList.contains('form-input__dropdown-elem-minus') ?
@@ -56,13 +62,61 @@
 
     function setInputText(elem){
 
-        let inputContainer = elem.closest('.form-input__container');
+        let inputText = '';
+        let inputContainer = elem.classList.contains('form-input__container') ?
+                elem :
+                elem.closest('.form-input__container');
+        
+        let dataText = inputContainer.getAttribute('data-text');
+        let listElems = inputContainer.querySelectorAll('.form-input__dropdown-elem');
+
+        if(dataText.length){
+
+            let totalCount = 0;
+            for(let i=0; i<listElems.length; i++){
+                totalCount+= +listElems[i].querySelector('.form-input__dropdown-elem-input').value;
+            }
+
+            inputText = totalCount + ' ' + dataText;
+
+        } else{
+
+            for(let i=0; i<listElems.length; i++){
+                let title = listElems[i].querySelector('.form-input__dropdown-elem-title').innerHTML;
+                let count = +listElems[i].querySelector('.form-input__dropdown-elem-input').value;
+                if(count){
+                    inputText += (inputText.length ? ', ' : '') + `${count} ${title}`;
+                }
+            }
+        }
+
+        
+
+        inputText = inputText.toLocaleLowerCase();
+        inputContainer.querySelector('.form-input__input').value = inputText;
+    }
+
+    function handleApplyClick(){
+        closeFormInput(this);
+    }
+
+    function handleClearClick(){
+        console.log(this);
+        let inputContainer = this.closest('.form-input__container');
         let listElems = inputContainer.querySelectorAll('.form-input__dropdown-elem');
 
         for(let i=0; i<listElems.length; i++){
-            
+            listElems[i].querySelector('.form-input__dropdown-elem-input').value = 0;
         }
-        console.log(listElems);
+
+        inputContainer.querySelector('.form-input__input').value = '';
+        closeFormInput(this);
+
+    }
+
+    function closeFormInput(elem){
+        let formInput = elem.closest('.form-input_mode_dropdown');
+        formInput.classList.remove('form-input_expanded');
     }
 
 })();
