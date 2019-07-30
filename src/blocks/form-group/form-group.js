e9e9af8c2145d52f4cb3e997106ba907142464b2
Artyom <artyom.baskakow@gmail.com>
2019-07-30 22:57:17 +0500

    Готова карточка card-search-room и на 90% готова карточка card-registration. Есть некоторые правки в изначальных компонентах для создания универсальности в верстке

--- src/blocks/form-group/form-group.js
+++ src/blocks/form-group/form-group.js
@@ -1,23 +1,23 @@
 (function(){
 
     let formLabels = document.querySelectorAll(
-        `.form-group_expandable_expanded .form-group__label,
-        .form-group_expandable_close .form-group__label`
+        `.form-group_expandable_expanded .form-label,
+        .form-group_expandable_close .form-label`
     );
 
     for(let i=0; i<formLabels.length; i++){
 
         formLabels[i].addEventListener('click', function(){
     
-            let parent = this.parentElement;
+            let formGroup = this.closest('.form-group');
         
-            if(parent.classList.contains('form-group_expandable_expanded')){
-                parent.classList.remove('form-group_expandable_expanded');
-                parent.classList.add('form-group_expandable_close');
+            if(formGroup.classList.contains('form-group_expandable_expanded')){
+                formGroup.classList.remove('form-group_expandable_expanded');
+                formGroup.classList.add('form-group_expandable_close');
             }
-            else if(parent.classList.contains('form-group_expandable_close')){
-                parent.classList.remove('form-group_expandable_close');
-                parent.classList.add('form-group_expandable_expanded');
+            else if(formGroup.classList.contains('form-group_expandable_close')){
+                formGroup.classList.remove('form-group_expandable_close');
+                formGroup.classList.add('form-group_expandable_expanded');
             }
         });
     }
