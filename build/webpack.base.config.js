c28ca57a4112d873eca2ef49bd1f803b4d43ae6f
Artyom <artyom.baskakow@gmail.com>
2019-08-19 18:37:04 +0500

    Правка путей к assets для github pages

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -9,7 +9,7 @@ const PATHS = {
     src: path.join(__dirname, '../src'),
     dist: path.join(__dirname, '../dist'),
     //assets: 'assets/'
-    assets: './assets/'
+    assets: 'assets/'
 }
 
 const PAGES_DIR = `${PATHS.src}/pages/`;
