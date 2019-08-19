bb491abd2563ccd40327a579d651e11f4f681d5d
Artyom <artyom.baskakow@gmail.com>
2019-08-19 17:12:02 +0500

    Правка для github pages

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -8,7 +8,8 @@ const webpack = require('webpack');
 const PATHS = {
     src: path.join(__dirname, '../src'),
     dist: path.join(__dirname, '../dist'),
-    assets: 'assets/'
+    //assets: 'assets/'
+    assets: './assets/'
 }
 
 const PAGES_DIR = `${PATHS.src}/pages/`;
